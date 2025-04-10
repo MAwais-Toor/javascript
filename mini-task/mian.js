39554a31bf44a7169e9e429ed9d34a16c3695d28
MAwais-Toor <mawaisakram2005@gmail.com>
2025-04-11 00:13:06 +0500

    Add mini task file in javascript folder

---
+++ mini-task/mian.js
@@ -0,0 +1,31 @@
+// =======Task 01======= //
+// Username and Password Verification
+
+let a=prompt("Enter your username")
+let b=prompt("Enter your Password")
+
+if(a=="admin" && b=="12345" ){
+    alert("Access granted")
+}else{
+    alert("Invalid credentials")
+}
+
+// =======Task 02======= //
+// Find the average of three numbers
+// let num1=prompt("Enter first number ");
+// let num2=prompt("Enter second number ");
+// let num3=prompt("Enter third number ");
+
+// let num4 = (num1 + num2 + num3) / 3;
+// alert("Average Value " + num4);
+
+// =======Task 03======= //
+// Find the largest of three numbers
+
+
+// =======Task 04======= //
+// Find the smallest of three numbers
+
+
+
+
