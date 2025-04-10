096381174b1bb58e8b33070c273228f05c02448c
MAwais-Toor <mawaisakram2005@gmail.com>
2025-04-10 12:39:46 +0500

    add js comparisons and string method file

---
+++ js comparisons/main.js
@@ -0,0 +1,33 @@
+
+
+// let a= 10;
+// if(a== 10){
+//     alert("Your condition is running")
+// }
+
+
+
+// let a =10;
+// let b= 2;
+// if(a+b== "2"){
+// document.write("your condition is true")
+// }
+// else{
+//     document.write("your condition is false")
+// }
+
+
+// let c=20;
+// if(c >"12"){
+//     document.write("your condition is true")
+// }else{
+//     document.write("your condition is false")
+// }
+
+
+let d = Number(prompt("Enter your number greater then 20"));
+if (d > "20") {
+    document.write("your condition is true")
+} else {
+    document.write("your condition is false")
+}
---
+++ string methods/main.js
@@ -0,0 +1,54 @@
+
+//====== Part 2 ======//
+
+
+// let arr1=['Pakistan','India','China','Bangladash']
+// console.log(arr1.length);
+
+// console.log(arr1.toString());
+
+// console.log(arr1[0]);
+
+// console.log(arr1.at(2));
+
+// console.log(arr1.join(' .'));
+
+// arr1.pop();
+
+// arr1.push('England');
+
+// arr1.shift();
+
+// arr1.unshift('Any Country');
+
+// delete arr1[1];
+
+
+// let arr2=['USA', 'UAE', 'Saudia Aribia','Newziland'];
+// let newarr= arr1.concat(arr2);
+// console.log(newarr);
+
+
+// let arr2=['USA', 'UAE', 'Saudia Aribia','Newziland','austria '];
+//  arr2.copyWithin(2,0);
+//  console.log(arr2);
+
+
+// const myArr = [[1,2],[3,4],[5,6]];
+// const newArr = myArr.flat();
+// console.log(newArr);
+
+
+// var arr=["Banana", "Apple", "Kiwi"];
+// var newarr=arr.splice(2,1,"onion","carrot");
+// console.log(newarr);  //show hidden value in console
+// console.log(arr);  //show value in console
+// console.log(newarr);  //show hidden value in console 
+
+
+// const mnth =['jan', 'feb', 'mar', 'apr']
+// const newmnth = mnth.toSpliced(1,1,"may","jun");
+// console.log(newmnth);
+// console.log(mnth);
+
+
